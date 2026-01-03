4c9304e1a6f5a12928079453b6ae4cbed6d1fccd
AirIHL <inhak1122@gmail.com>
2026-01-03 15:17:48 +0900

    first commit

---
+++ src/main/java/com/example/springjpapractice1/SpringJpaPractice1Application.java
@@ -0,0 +1,13 @@
+package com.example.springjpapractice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaPractice1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaPractice1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springjpapractice1/SpringJpaPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjpapractice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaPractice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
